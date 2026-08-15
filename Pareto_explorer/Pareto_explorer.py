70f004f303d626138f5d91b4aab9bde9b1a28e46
moeabench-rgb <moeabench@gmail.com>
2026-08-15 14:16:08 -0300

    df

--- Pareto_explorer/Pareto_explorer.py
+++ Pareto_explorer/Pareto_explorer.py
@@ -1,11 +1,11 @@
-class Pareto_explorer:
+class pareto_explorer:
 
   def __init__(self,population):
     self.population = population
 
   def executing(self):
 
-    pareto = Pareto_dominance()
+    pareto = pareto_dominance()
     ranks = pareto.rank_pareto(self.population)
     idx_rank = list(dict.fromkeys(pareto.idx_rank(self.population,ranks)))
     idx_aux = pareto.associate_rank(ranks,idx_rank)
