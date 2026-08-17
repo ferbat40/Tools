805607261c9daf06afd7b9610d289023a6b8c3e6
moeabench-rgb <moeabench@gmail.com>
2026-08-17 16:16:57 -0300

    sdf

--- Pareto_explorer/coordnates.py
+++ Pareto_explorer/coordnates.py
@@ -9,44 +9,49 @@ class coordnates:
 
  def __init__(self, variables):
           self.variables=variables
-     
-
+          
 
  def configure(self, idx = None):
-  if idx is not None:
-    var = np.array([self.variables[idx-i]   for i in range(0,10)   ]  )
+  lim = [idx,self.variables.shape[0]]
+  prev = np.sort([lim[0]-i  for i in range(0,11) if lim[0]-i >=0 and lim[0]-i != lim[0]])
+  point_idx = np.array([idx])
+  next = [lim[0]+i  for i in range(0,11) if lim[0]+i < lim[1]  and lim[0]+i != lim[0]]
+  var_idx  = []
+  if len(prev) > 0 and len(next) > 0:
+    var_idx = np.concatenate((prev,point_idx,next))
+  elif len(prev) == 0 and len(next) > 0:
+    var_idx = np.concatenate((point_idx,next))
+  elif len(prev) > 0 and len(next) == 0:
+    var_idx = np.concatenate((prev,point_idx))
 
+  if idx is not None:
     arr = [
     dict(
         label=f'X{b+1}',
-        values=var[:, b],
+        values=self.variables[var_idx,b],
         range=[0, 1]
     )
-    for b in range(var.shape[1])
-]
-    #arr = [ dict(label = f'X{b+1}', values = np.array(var[:,b])  , range = [np.min(pop[:,b]),np.max(pop[:,b])   ]) for b in range(0,var.shape[1])]
-    colors = np.zeros(var.shape[0])
-    colors[0]=1
-  fig = go.Figure()
-
-  fig.add_trace(
-
-   go.Parcoords(
+    for b in range(self.variables.shape[1])]
+    colors = np.zeros(len(var_idx))
+    pos = np.where(var_idx == idx)[0][0]
+    colors[pos]=1
+    fig = go.Figure()
+    fig.add_trace(
+     go.Parcoords(
         line = dict(
 
           color = colors,
           colorscale=[
-        [0.0, 'lightgray'],
-        [0.99, 'lightgray'],
+        [0.0, 'gray'],
         [1.0, 'red']
     ]
         ),
         dimensions=arr
     ))
-  fig.update_layout(
+    fig.update_layout(
 
                      width = 1300,
-                     height=700,
+                     height=500,
                      title=dict(
                      text=f'Decision variables for solution {idx}',
                      x=0.5,
